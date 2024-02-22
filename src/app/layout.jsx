8be2cde10b711998ebd39e2a43a2248be2d58328
blonardi <lonardibautista@gmail.com>
2024-02-22 17:54:10 -0300

    Fix background

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+			<div className="fixed inset-0 z-[-2] h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
 				<body className={onest.className}>{children}</body>
 			</div>
 		</html>
