bc156b11d3bc6ea46fd86c8d115fe5891f1c9bc9
blonardi <lonardibautista@gmail.com>
2024-03-04 17:08:17 -0300

    Fix colors and imports on project section

--- src/app/(index)/sections/Projects/Projects.jsx
+++ src/app/(index)/sections/Projects/Projects.jsx
@@ -1,61 +1,61 @@
-'use client'
 import react from '@/app/assets/icons/react.svg'
 import next from '@/app/assets/icons//next.svg'
-import bootstrap from '@/app/assets/icons//bootstrap.svg'
-import cheerio from '@/app/assets/icons//cheerio.svg'
-import tailwind from '@/app/assets/icons//tailwind.svg'
-import javascript from '@/app/assets/icons//javascript.svg'
-import mongodb from '@/app/assets/icons//mongodb.svg'
-import express from '@/app/assets/icons//express.svg'
-import github from '@/app/assets/icons//github.svg'
-import code from '@/app/assets/icons//code.svg'
+import bootstrap from '@/app/assets/icons/bootstrap.svg'
+import cheerio from '@/app/assets/icons/cheerio.svg'
+import tailwind from '@/app/assets/icons/tailwind.svg'
+import javascript from '@/app/assets/icons/javascript.svg'
+import mongodb from '@/app/assets/icons/mongodb.svg'
+import express from '@/app/assets/icons/express.svg'
+import githubIcon from '@/app/assets/icons/github.svg'
+import codeIcon from '@/app/assets/icons/code.svg'
 //images
 import argynews from '@/app/assets/images-projects/argynews-card.webp'
 import cultura from '@/app/assets/images-projects/cultura-card.webp'
 
 import Image from "next/image"
+import SocialPill from '../Hero/SocialPill'
 
 export default function ProjectList() {
 	const TAGS = {
 		//HTML:{name:"",class:"",icon:()},
 		REACT: {
 			name: 'React',
-			class: 'bg-[rgb(8 126 164)] text-white',
+			class: 'bg-[#475569] text-white',
 			icon: react,
 		},
 		NEXT: {
 			name: 'Next.js',
-			class: 'bg-black text-white',
+			class: 'bg-[#475569] text-white',
 			icon: next
 		},
 		JAVASCRIPT: {
 			name: 'Javascript',
-			class: 'bg-black text-[#323330]',
+			class: 'bg-[#475569] text-white',
 			icon: javascript
 		},
 		BOOTSTRAP: {
 			name: 'Bootstrap',
-			class: 'bg-[#4c0bce] text-white',
+			class: 'bg-[#475569] text-white',
 			icon: bootstrap,
 		},
 		CHEERIO: {
 			name: 'Cheerio',
-			class: 'bg-black text-[rgb(232, 140, 31)]',
+			class: 'bg-[#475569] text-[rgb(232, 140, 31)]',
 			icon: cheerio
 		},
 		TAILWIND: {
 			name: 'TailwindCSS',
-			class: 'bg-white text-[#73bcf6]',
+			class: 'bg-[#475569] text-[#73bcf6]',
 			icon: tailwind
 		},
 		EXPRESS: {
 			name: 'Express',
-			class: 'bg-light-gray text-white',
+			class: 'bg-[#475569] text-white',
 			icon: express,
 		},
 		MONGODB: {
 			name: 'MongoDB',
-			class: 'bg-white text-[#01EC64]',
+			class: 'bg-[#475569] text-[#01EC64]',
 			icon: mongodb
 		}
 	}
@@ -95,30 +95,34 @@ export default function ProjectList() {
 	]
 	return (
 		<>
-			<section className="grid items-center justify-center grid-cols-1 gap-8 rounded-lg md:grid-cols-2">
+			<section className="grid items-start justify-center grid-cols-1 gap-8 rounded-lg md:grid-cols-2">
 				{PROJECTS.map(({ index, title, description, link, github, img, technologies }) => (
-					<article key={index} className="overflow-hidden shadow-xl shadow-slate-100/10 ">
+					<article key={index} className="h-full overflow-hidden shadow-xl shadow-slate-100/10">
 						<a href={link}>
 							<Image width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} className="rounded-lg" src={img} alt={`${title} img`} />
 						</a>
-						<div className="p-2 text-wrap">
-							<h3 className="mb-2 text-2xl font-semibold text-orange-300">{title}</h3>
-							<ul className="flex items-center p-2 mb-2 text-center rounded-sm shadow-xl bg-slate-100 shadow-slate-100/10 gap-x-4">
-								{technologies.map(tech => (
-									//console.log(tech)
-									<li key={tech.name}>
-										<span className={`flex gap-x-2 rounded-full ${tech.class}`}>
-											<Image src={tech.icon} width={24} height={24} alt={tech.name} />
-										</span>
-									</li>
+						<div className="flex flex-col justify-between gap-2 p-4 text-wrap">
+							<h3 className="text-2xl font-semibold text-orange-300">{title}</h3>
+							<div className='h-full min-h-24 grow'>
+								<ul className="flex flex-wrap items-center p-2 mb-2 text-center rounded-sm gap-y-2 gap-x-6 ">
+									{technologies.map(tech => (
+										//console.log(tech)
+										<li className={`rounded-full shadow-xl shadow-slate-600 ${tech.class}`} key={tech.name}>
+											<div className='flex items-center justify-center px-2 py-1 gap-x-1'>
+												<Image src={tech.icon} width={24} height={24} alt={tech.name} />
+												<span className={`rounded-full gap-x-2 `}>
+													{tech.name}
+												</span>
+											</div>
+										</li>
 
-								))}
-							</ul>
-
+									))}
+								</ul>
+							</div>
 							<p className="text-lg text-pretty">{description}</p>
 							<div className='flex items-center justify-around gap-x-4'>
-								<span>Code</span>
-								<span>Deploy</span>
+								<SocialPill link={github}><Image src={codeIcon} width={24} height={24} alt="Link to Github repo" /><span>Code</span></SocialPill>
+								<SocialPill link={link}><Image src={githubIcon} width={24} height={24} alt="Link to deploy" /><span>Deploy</span></SocialPill>
 							</div>
 						</div>
 					</article>
