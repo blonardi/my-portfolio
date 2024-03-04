29415f4cf48845313670aab7bb2584ff21e612ab
blonardi <lonardibautista@gmail.com>
2024-03-04 17:07:10 -0300

    Add changes to SocialPill

--- src/app/(index)/sections/Hero/SocialPill/index.jsx
+++ src/app/(index)/sections/Hero/SocialPill/index.jsx
@@ -1,11 +1,11 @@
 import { Children } from "react";
 
-export default function SocialPill({link, children}){
-	return(
+export default function SocialPill({ link, children }) {
+	return (
 		<>
-			<a href={link} className="rounded-full border border-white/10 flex justify-center items-center gap-x-2 py-2 px-2 bg-white/5 hover:scale-105 hover:bg-white/10 transition">
+			<a href={link} target='_blank' className="flex items-center justify-center px-2 py-2 transition border rounded-full cursor-pointer border-white/10 gap-x-2 bg-white/5 hover:scale-105 hover:bg-white/10">
 				{children}
-			</a>	
+			</a>
 		</>
 	)
 }
