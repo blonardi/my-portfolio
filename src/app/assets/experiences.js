edf3aae06ee469b35b5dccd74ec2149c02434aa7
blonardi <lonardibautista@gmail.com>
2024-03-05 16:27:53 -0300

    Add experiences to an object for iterate it on different files

---
+++ src/app/assets/experiences.js
@@ -0,0 +1,28 @@
+import kumpel from '@/app/assets/images-projects/kumpel.webp'
+import tubatech from '@/app/assets/images-projects/tubatech.webp'
+const EXPERIENCE = [
+	{
+		id: 1,
+		image: kumpel,
+		href: 'https://kumpel.com.ar/',
+		enterprise: 'Kumpel',
+		date: 'March 22 - Present',
+		title: 'Fullstack Developer',
+		subtitle: 'Development and design of websites with React and Express stack.',
+		description: 'I specialize in the analysis, design, and development of websites. From the initial deployment to ongoing maintenance, i have applied my skills in HTML, CSS, Bootstrap, Astro, React, and JavaScript. Using agile methodologies like Scrum, I have ensured efficiency in development and focused on creating responsive and engaging web experiences. My scope also extended to the deployment and maintenance of sites, ensuring their performance and security over time. Kumpel provided me with the opportunity to refine my skills and tackle challenges in web development.',
+		link: true,
+	},
+	{
+		id: 2,
+		image: tubatech,
+		href: 'https://www.tuba-tech.com.ar/',
+		enterprise: 'Tuba-Tech',
+		date: 'March 21 - January 23',
+		title: 'Frontend Developer',
+		subtitle: 'Development and design of websites with Wordpress and PHP stack.',
+		description: 'As a Frontend Developer at Tuba-tech, I contributed to the development of a comprehensive system for managing and registering members in a social club like Club Central Larroque. I also handled the evolutionary and corrective maintenance of the frontend for the Juan Bautista Alberdi Library website, enhancing the user experience. Applying my skills in HTML5, CSS, Bootstrap, Wordpress, PHP, Javascript, SQL, and XML, I utilized tools such as GIT, Github, Figma, and SVN for efficient collaboration. My time at Tuba-tech provided me with a valuable initial experience filled with new knowledge.',
+		link: true,
+	},
+]
+
+export default EXPERIENCE
