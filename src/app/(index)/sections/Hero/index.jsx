d0267e1f06bd636eec9ad85ba22c2ecb5b59b07b
blonardi <lonardibautista@gmail.com>
2024-03-05 16:30:19 -0300

    Add blinking light

--- src/app/(index)/sections/Hero/index.jsx
+++ src/app/(index)/sections/Hero/index.jsx
@@ -1,4 +1,5 @@
 'use client'
+import './hero.css'; // Asegúrate de importar el archivo de estilo
 import Image from 'next/image'
 import profilePic from '@/app/assets/blonardi-profile.jpeg'
 import SocialPill from './SocialPill'
@@ -16,7 +17,7 @@ export default function Hero() {
 				</figure>
 				<h1 className="flex flex-row pb-6 text-2xl font-bold text-white lg:text-5xl gap-x-4">Hey, i am Bautista<a href="https://www.linkedin.com/in/bautista-lonardi/" className="flex items-center justify-center transition hover:scale-105"
 					target="_blank"
-				><span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-1 rounded dark:bg-gray-700 dark:text-gray-300 text-pretty">Available to work</span></a>
+				><span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-1 rounded dark:bg-gray-700 dark:text-gray-300 text-pretty"><span className="blinking-circle"></span> Available to work</span></a>
 				</h1>
 				<h2 className="text-lg text-justify lg:text-2xl opacity-90">Fullstack developer based in Entre Rios, <span className='inline-block bg-gradient-to-r from-[#73ACDF] via-[#fff] to-[#73ACDF] bg-clip-text text-transparent'>Argentina</span>. I specialized in create special and unique web apps. My stack is Next, Express, some Python and SQL too.. </h2>
 
