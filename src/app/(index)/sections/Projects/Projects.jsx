652dc282f6d47393437b2ab68679da763ed4b4b4
blonardi <lonardibautista@gmail.com>
2024-03-05 17:17:13 -0300

    Add hover style to article

--- src/app/(index)/sections/Projects/Projects.jsx
+++ src/app/(index)/sections/Projects/Projects.jsx
@@ -97,9 +97,17 @@ export default function ProjectList() {
 		<>
 			<section className="grid items-start justify-center grid-cols-1 gap-8 rounded-lg md:grid-cols-2">
 				{PROJECTS.map(({ index, title, description, link, github, img, technologies }) => (
-					<article key={index} className="h-full overflow-hidden shadow-xl shadow-slate-100/10">
-						<a href={link}>
-							<Image width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} className="rounded-lg" src={img} alt={`${title} img`} />
+					<article key={index} className="h-full overflow-hidden transition-all duration-500 transform shadow-md hover:scale-105 hover:shadow-lg shadow-slate-100/10">
+						<a className='relative block h-[200px]' href={link}>
+							<Image
+								width={0}
+								height={0}
+								sizes="100vw"
+								style={{ width: '100%', height: '200px' }}
+								className="object-cover rounded-lg"
+								src={img}
+								alt={`${title} img`}
+							/>
 						</a>
 						<div className="flex flex-col justify-between gap-2 p-4 text-wrap">
 							<h3 className="text-2xl font-semibold text-orange-300">{title}</h3>
@@ -107,7 +115,7 @@ export default function ProjectList() {
 								<ul className="flex flex-wrap items-center p-2 mb-2 text-center rounded-sm gap-y-2 gap-x-6 ">
 									{technologies.map(tech => (
 										//console.log(tech)
-										<li className={`rounded-full shadow-xl shadow-slate-600 ${tech.class}`} key={tech.name}>
+										<li className={`rounded-full shadow-md shadow-slate-500 ${tech.class}`} key={tech.name}>
 											<div className='flex items-center justify-center px-2 py-1 gap-x-1'>
 												<Image src={tech.icon} width={24} height={24} alt={tech.name} />
 												<span className={`rounded-full gap-x-2 `}>
