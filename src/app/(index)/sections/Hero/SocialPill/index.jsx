e19ed9377705c51c30b097b8567f5a5f7b9cf28d
blonardi <lonardibautista@gmail.com>
2024-03-05 17:16:55 -0300

    Change hover pill

--- src/app/(index)/sections/Hero/SocialPill/index.jsx
+++ src/app/(index)/sections/Hero/SocialPill/index.jsx
@@ -3,7 +3,7 @@ import { Children } from "react";
 export default function SocialPill({ link, children }) {
 	return (
 		<>
-			<a href={link} target='_blank' className="flex items-center justify-center px-2 py-2 transition border rounded-full cursor-pointer border-white/10 gap-x-2 bg-white/5 hover:scale-105 hover:bg-white/10">
+			<a href={link} target='_blank' className="flex items-center justify-center px-2 py-2 transition duration-500 border rounded-full cursor-pointer border-white/10 gap-x-2 bg-white/5 hover:scale-105 hover:bg-white/10">
 				{children}
 			</a>
 		</>
