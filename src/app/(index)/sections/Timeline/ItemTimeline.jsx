9f9bd58cc6e440feb2bc4e57e5c3391bc27d35b6
blonardi <lonardibautista@gmail.com>
2024-03-05 16:29:30 -0300

    Add changes to timeline

--- src/app/(index)/sections/Timeline/ItemTimeline.jsx
+++ src/app/(index)/sections/Timeline/ItemTimeline.jsx
@@ -1,4 +1,6 @@
-export default function ItemTimeline({ enterprise, date, title, description, link }) {
+import Link from "next/link";
+
+export default function ItemTimeline({ id, enterprise, date, title, description, link }) {
 	return (
 		<>
 			<div>
@@ -7,9 +9,12 @@ export default function ItemTimeline({ enterprise, date, title, description, lin
 				<h3 className="mt-3 text-lg font-semibold text-orange-800 dark:text-white">{title}</h3>
 				<p className="mb-4 text-base font-normal text-sky-100 dark:text-gray-400 text-pretty">{description}</p>
 				{link &&
-					<a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-						<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-					</svg></a>
+
+					<Link
+						href={`/experience/${id}`} className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Detalles <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+							<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+						</svg>
+					</Link>
 				}
 			</div>
 		</>
