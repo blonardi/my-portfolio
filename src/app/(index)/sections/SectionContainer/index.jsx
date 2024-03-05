3458b3d49205f257717b00379551bc15116acd42
blonardi <lonardibautista@gmail.com>
2024-03-05 16:31:24 -0300

    Simplify component Section

--- src/app/(index)/sections/SectionContainer/index.jsx
+++ src/app/(index)/sections/SectionContainer/index.jsx
@@ -1,12 +1,9 @@
-'use client'
-export default function SectionContainer({children, id, newClass}){
+export default function SectionContainer({ children, id, newClass }) {
 	return (
 		<>
-			<div>
-				<section id={id} className={`w-full px-8 lg:w-full lg:max-w-screen-md mx-auto ${newClass}`}>
-					{children}
-				</section>
-			</div>
+			<section id={id} className={`w-full px-8 lg:w-full lg:max-w-screen-md mx-auto ${newClass}`}>
+				{children}
+			</section>
 		</>
 	)
 }
