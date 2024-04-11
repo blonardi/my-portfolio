b28635e5a6683cac9717772baeead9fbcc24b18f
blonardi <lonardibautista@gmail.com>
2024-04-11 16:47:43 -0300

    Add supressHydration

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<head>
 				<link rel="icon" href="/favicon.ico" sizes="any" />
 				<link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>" />
@@ -20,9 +20,10 @@ export default function RootLayout({ children }) {
 					type="image/<generated>"
 					sizes="<generated>" />
 			</head>
-			<div className="fixed inset-0 z-[-2] h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-				<body className={onest.className}>{children}</body>
-			</div>
+			<body className={onest.className}>
+				<div className='fixed inset-0 z-[-2] h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'>{children}
+				</div>
+			</body>
 		</html>
 	)
 }
