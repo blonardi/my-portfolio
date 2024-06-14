66d9976793d3211e318193800ae74814e3ec5a0a
blonardi <lonardibautista@gmail.com>
2024-06-14 19:08:43 -0300

    Fix description experience

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
 					sizes="<generated>" />
 			</head>
 			<body className={onest.className}>
-				<div className='fixed inset-0 z-[-2] h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'>{children}
+				<div className='inset-0 z-[-2] h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'>{children}
 				</div>
 			</body>
 		</html>
